2be2d78ad51829f76363dd6dcdc468b14477c124
Gabrielli-Rech <gabbyrech133@gmail.com>
2024-09-02 10:00:06 -0300

    teste push e pull

--- nbproject/exercios/NotaEscolar.java
+++ nbproject/exercios/NotaEscolar.java
@@ -6,28 +6,36 @@ public class NotaEscolar {
      * Tendo 7 como a média dos alunos:
      * @param args
      */
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        float nota[] = new float[5];
-        String aluno, turma, resultado;
-            System.out.println("|< Média dos Alunos >|");
-            System.out.print("Informe o nome do(a) aluno(a): ");
-                aluno = ler.nextLine();
-            System.out.print("Informe a turma do(a) aluno(a): ");
-                turma = ler.nextLine();
-        for(int i = 0; i < 4; i++){
-            System.out.print("Informe a " + (i + 1) + "° nota: ");
-                nota[i] = ler.nextFloat();
-                nota[4]  += nota [i];
+        String alunos[][] = new String[5][2];
+        float notas [][] = new float [5][6];
+        System.out.println(".: Notas Escolares 2 :.");
+        for (int i = 0; i < 5; i++){
+            System.out.print("Informe o nome " + (i+1) + "° Aluno ");
+            alunos[i][0] = ler.next();
+            for ( int j = 0; j < 4; j++){
+                System.out.print("digite a " + (j+1) + "ª nota: ");
+                notas[i][j] = ler.nextFloat();
+                notas[i][5] += notas[i][j]; // acumula notas
+            }//fim for notas
+            notas[i][5] = notas[i][5] / 4;
+            System.out.print("Digite a Frequancia do aluno " + alunos[i][0] + ": ");
+            notas[i][4] = ler.nextFloat();
+            if (notas[i][4] < 75 ) {
+                alunos[i][1] = "reprovado por falta";
+            }else if (notas [i][5] < 7) {
+                alunos[i][1] = "Reprovado por nota";
+            }else {
+                alunos[i][1] = "aprovado";
             }
-            nota[4] = nota[4] / 4;
-        if (nota[4] >= 7){
-            System.out.println("Aprovado!");
-        }else if (nota[4] <= 5){
-            System.out.println("Recuperação!");
-        }else{
-            System.out.println("Reprovado!");
+            System.out.println("-----------");
+        }// fim do for
+        for (int i = 0; i < 5; i++){
+            System.out.println("o aluno " + alunos[i][0] + " teve a média de " + notas[i][5] + " frequencia: " + notas[i][4] + " e foi "+ alunos[i][1] + ".");
         }
-            System.out.println("O aluno(a) " + aluno + " está com a média " + nota[4] );
-    }
+        System.out.println("sistema encerrado");
+        ler.close();
+        System.out.println("Alterado no VS online");
+        }
 }
